63d086b4989637c531d7d1792eeab7f5c46a0837
Jainam004747 <jainamjainampatel1234@gmail.com>
2022-07-07 15:43:26 +0530

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [noOfClick, setNoOfClick] = useState(0);
+
+  const incr = (no) => {
+    setTotal(total + no);
+    setNoOfClick(noOfClick + 1);
+  };
   return (
     <div className="App">
-    
+      
     </div>
   );
 }
